11845cb136b75211a86898a568aa134039a5547c
Michele Gentile <michele.gentile2604@gmail.com>
2024-12-09 15:08:10 +0100

    actualized data_loading

--- R/data_loading.R
+++ R/data_loading.R
@@ -5,23 +5,44 @@
 # Load required libraries
 library(tidyverse)
 library(sf)
+library(tmap)
 
 # Function to load and preprocess data
 load_airbnb_data <- function() {
     # Load listings data
-    load("data/") # INSERT CORRECT DATAS
+    load("clean_data/mallorca/listing_common0_select.RData")
+    ls()
+
+    str(listings_common0_select)
+
+    unique(listings_common0_select$date)
     
     # Read reviews data
-    reviews <- read_csv("data/") %>% # INSERT CORRECT DATAS
+    reviews = read_csv("data/mallorca/2023-12-17/reviews.csv.gz")
+    str(reviews)
+
+    head(reviews)
         # Basic preprocessing
-        mutate(
-        comment_length = str_count(comments, "\\w+"),
-        date = as.Date(date)
-        )
+#        mutate(
+#        comment_length = str_count(comments, "\\w+"),
+#        date = as.Date(date)
+#        )
     
     # Read neighbourhoods
-    municipios <- read_csv("data/") # INSERT CORRECT DATAS
+    municipios = read_csv("data/mallorca/2023-12-17/neighbourhoods.csv")
+    str(municipios)
     
+    head(municipios)
+
+    # Leer el archivo GeoJSON
+    geojson_sf <- sf::st_read("data/mallorca/2024-09-13/neighbourhoods.geojson")
+
+    # Crear un mapa
+
+    # interactivo
+    tmap_mode("plot") # Cambiar a modo  view/plot   que es interactivo/estático
+    tm_shape(geojson_sf) + tm_polygons(col = "cyan", alpha = 0.6) + tm_layout(title = "Mapa - GeoJSON Mallorca con municipios")
+
     # Return list of processed datasets
     list(
         listings = listings_common0_select,
